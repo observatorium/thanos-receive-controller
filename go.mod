1fefce989ab4eae471af090db925ade41c723466
Matthias Loibl <mail@matthiasloibl.com>
2019-06-17 19:15:46 +0200

    Send updates to config decodes as hashringconfig via channel

--- go.mod
+++ go.mod
@@ -3,23 +3,23 @@ module github.com/observatorium/thanos-receive-controller
 go 1.12
 
 replace (
-	github.com/coreos/prometheus-operator => github.com/coreos/prometheus-operator v0.30.1
-	k8s.io/api => k8s.io/api v0.0.0-20190606204050-af9c91bd2759
-	k8s.io/client-go => k8s.io/client-go v11.0.1-0.20190606204521-b8faab9c5193+incompatible
+	github.com/improbable-eng/thanos => github.com/squat/thanos v0.5.0-rc.0.0.20190617145307-251a6da0d967
+	k8s.io/client-go => k8s.io/client-go v0.0.0-20190615125933-7de88b14dcc8
 )
 
 require (
+	github.com/go-kit/kit v0.8.0
 	github.com/google/gofuzz v1.0.0 // indirect
+	github.com/googleapis/gnostic v0.3.0 // indirect
 	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/improbable-eng/thanos v0.5.0
 	github.com/oklog/run v1.0.0
-	github.com/spf13/pflag v1.0.3 // indirect
 	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8 // indirect
 	golang.org/x/net v0.0.0-20190613194153-d28f0bde5980 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
-	k8s.io/api v0.0.0-00010101000000-000000000000 // indirect
-	k8s.io/apimachinery v0.0.0-20190612125636-6a5db36e93ad // indirect
-	k8s.io/client-go v0.0.0-00010101000000-000000000000
-	k8s.io/klog v0.3.3 // indirect
-	k8s.io/utils v0.0.0-20190607212802-c55fbcfc754a // indirect
+	gopkg.in/yaml.v2 v2.2.2
+	k8s.io/api v0.0.0-20190614205929-e4e27c96b39a
+	k8s.io/apimachinery v0.0.0-20190612125636-6a5db36e93ad
+	k8s.io/client-go v2.0.0-alpha.0.0.20181121191925-a47917edff34+incompatible
 )
--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	stdlog "log"
+	"os"
 
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/improbable-eng/thanos/pkg/receive"
 	"github.com/oklog/run"
+	"gopkg.in/yaml.v2"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -27,32 +35,100 @@ func main() {
 	flag.StringVar(&config.ConfigMapGeneratedName, "configmap-generated-name", "", "The name to the generated and populated ConfigMap")
 	flag.Parse()
 
+	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.WithPrefix(logger, "ts", log.DefaultTimestampUTC)
+	logger = log.WithPrefix(logger, "caller", log.DefaultCaller)
+
+	konfig, err := clientcmd.BuildConfigFromFlags("", config.KubeConfig)
+	if err != nil {
+		stdlog.Fatal(err)
+	}
+
+	klient, err := kubernetes.NewForConfig(konfig)
+	if err != nil {
+		stdlog.Fatal(err)
+	}
+
+	hashringUpdates := make(chan []receive.HashringConfig)
+
 	var gr run.Group
 	{
-		// Watch the original ConfigMap
-	}
-	{
-		konfig, err := clientcmd.BuildConfigFromFlags("", config.KubeConfig)
-		if err != nil {
-			stdlog.Fatal(err)
-		}
-
-		klient, err := kubernetes.NewForConfig(konfig)
-		if err != nil {
-			stdlog.Fatal(err)
-		}
-
-		controller := &Controller{
-			klient,
-		}
+		ctx, cancel := context.WithCancel(context.Background())
+		cmw := ConfigMapWatcher{klient: klient}
 
 		gr.Add(func() error {
-			return controller.Run(config.Namespace, config.StatefulSetLabel)
+			return cmw.Watch(ctx, config.Namespace, config.ConfigMapName, hashringUpdates)
 		}, func(err error) {
-			controller.Shutdown()
+			cancel()
 		})
 	}
+	{
+		gr.Add(func() error {
+			for hu := range hashringUpdates {
+				for _, hashring := range hu {
+					fmt.Printf("%+v\n", hashring)
+				}
+			}
+			return nil
+		}, func(err error) {
+		})
+	}
+	//{
+	//	controller := &Controller{
+	//		klient,
+	//	}
+	//
+	//	gr.Add(func() error {
+	//		return controller.Run(config.Namespace, config.StatefulSetLabel)
+	//	}, func(err error) {
+	//		controller.Shutdown()
+	//	})
+	//}
 
+	level.Info(logger).Log("msg", "starting the controller")
+
+	if err := gr.Run(); err != nil {
+		stdlog.Fatal(err)
+	}
+}
+
+type ConfigMapWatcher struct {
+	klient kubernetes.Interface
+	logger log.Logger
+}
+
+func (cmw *ConfigMapWatcher) Watch(ctx context.Context, namespace string, name string, updates chan<- []receive.HashringConfig) error {
+	if cmw.logger == nil {
+		cmw.logger = log.NewNopLogger()
+	}
+
+	watch, err := cmw.klient.CoreV1().ConfigMaps(namespace).Watch(metav1.ListOptions{
+		FieldSelector: fields.OneTermEqualSelector("metadata.name", name).String(),
+		Watch:         true,
+	})
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			watch.Stop()
+			close(updates)
+		case event := <-watch.ResultChan():
+			cm := event.Object.(*corev1.ConfigMap)
+			c := cm.Data["config.yaml"]
+
+			var hashrings []receive.HashringConfig
+
+			if err := yaml.Unmarshal([]byte(c), &hashrings); err != nil {
+				level.Warn(cmw.logger).Log("msg", "failed to decode configuration", "err", err)
+				return err
+			}
+
+			updates <- hashrings
+		}
+	}
 }
 
 type Controller struct {
@@ -64,6 +140,7 @@ func (c *Controller) Run(namespace string, statefulSetLabel string) error {
 		LabelSelector: statefulSetLabel,
 	})
 	if err != nil {
+		return err
 	}
 
 	for _, sts := range list.Items {
@@ -74,6 +151,4 @@ func (c *Controller) Run(namespace string, statefulSetLabel string) error {
 	return nil
 }
 
-func (c *Controller) Shutdown() {
-
-}
+func (c *Controller) Shutdown() {}
