45a19edf5549b9c2bd5ff148427cc9b1133e3616
Lucas Servén Marín <lserven@gmail.com>
2019-06-25 15:01:17 +0200

    main.go: move to informer pattern
    This commit refactors the controller to use the standard k8s informer
    pattern.

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	stdlog "log"
 	"os"
 	"os/signal"
 	"strings"
+	"time"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/improbable-eng/thanos/pkg/receive"
 	"github.com/oklog/run"
+	"github.com/pkg/errors"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	appsinformers "k8s.io/client-go/informers/apps/v1"
+	coreinformers "k8s.io/client-go/informers/core/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	resyncPeriod = 5 * time.Minute
 )
 
 func main() {
@@ -24,6 +39,7 @@ func main() {
 		StatefulSetLabel       string
 		ConfigMapName          string
 		ConfigMapGeneratedName string
+		FileName               string
 		Path                   string
 		Port                   int
 		Scheme                 string
@@ -34,6 +50,7 @@ func main() {
 	flag.StringVar(&config.StatefulSetLabel, "statefulset-label", "controller.receive.thanos.io=thanos-receive-controller", "The label StatefulSets must have to be watched by the controller")
 	flag.StringVar(&config.ConfigMapName, "configmap-name", "", "The name of the original ConfigMap containing the hashring tenant configuration")
 	flag.StringVar(&config.ConfigMapGeneratedName, "configmap-generated-name", "", "The name of the generated and populated ConfigMap")
+	flag.StringVar(&config.FileName, "file-name", "", "The name of the configuration file in the ConfigMap")
 	flag.StringVar(&config.Path, "path", "/api/v1/receive", "The URL path on which receive components accept write requests")
 	flag.IntVar(&config.Port, "port", 19291, "The port on which receive components are listening for write requests")
 	flag.StringVar(&config.Scheme, "scheme", "http", "The URL scheme on which receive components accept write requests")
@@ -42,6 +59,12 @@ func main() {
 	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
 	logger = log.WithPrefix(logger, "ts", log.DefaultTimestampUTC)
 	logger = log.WithPrefix(logger, "caller", log.DefaultCaller)
+
+	parts := strings.Split(config.StatefulSetLabel, "=")
+	if len(parts) != 2 {
+		stdlog.Fatal("--statefulset-label must be of the form 'key=value'")
+	}
+	labelKey, labelValue := parts[0], parts[1]
 
 	konfig, err := clientcmd.BuildConfigFromFlags("", config.KubeConfig)
 	if err != nil {
@@ -53,141 +76,229 @@ func main() {
 		stdlog.Fatal(err)
 	}
 
-	// The entire controller is synchronized via these 2 channels.
-	// The ConfigMapWatcher and StatefulSetWatcher write into individual channels.
-	// Both channels are read from the XXX and merges into the populated config which is then written.
-
-	hashringUpdates := make(chan []receive.HashringConfig)
-	StatefulSetUpdates := make(chan StatefulSetUpdate)
-
-	var gr run.Group
+	var g run.Group
 	{
-		gr.Add(func() error {
-			// Signal chans must be buffered.
-			sig := make(chan os.Signal, 1)
+		// Signal chans must be buffered.
+		sig := make(chan os.Signal, 1)
+		g.Add(func() error {
 			signal.Notify(sig, os.Interrupt, os.Kill)
 			<-sig
 			return nil
-		}, func(err error) {})
-	}
-	{
-		ctx, cancel := context.WithCancel(context.Background())
-		cmw := ConfigMapWatcher{klient: klient}
-
-		gr.Add(func() error {
-			return cmw.Watch(ctx, config.Namespace, config.ConfigMapName, hashringUpdates)
 		}, func(err error) {
-			level.Info(logger).Log("msg", "shutting down ConfigMap watcher")
-			cancel()
+			level.Info(logger).Log("msg", "caught interrrupt")
+			close(sig)
 		})
 	}
 	{
-		ctx, cancel := context.WithCancel(context.Background())
-		stsw := StatefulSetWatcher{klient: klient}
-
-		gr.Add(func() error {
-			return stsw.Watch(ctx, config.Namespace, config.StatefulSetLabel, StatefulSetUpdates)
-		}, func(err error) {
-			level.Info(logger).Log("msg", "shutting down StatefulSet watcher")
-			cancel()
-		})
-	}
-	{
-		ctx, cancel := context.WithCancel(context.Background())
-
-		cp := ConfigPopulator{
-			namespace:    config.Namespace,
-			statefulsets: map[string]StatefulSetUpdate{},
-			path:         config.Path,
-			port:         config.Port,
-			scheme:       config.Scheme,
+		opt := &options{
+			configMapName:          config.ConfigMapName,
+			configMapGeneratedName: config.ConfigMapGeneratedName,
+			fileName:               config.FileName,
+			namespace:              config.Namespace,
+			path:                   config.Path,
+			port:                   config.Port,
+			scheme:                 config.Scheme,
+			labelKey:               labelKey,
+			labelValue:             labelValue,
 		}
+		c := newController(klient, logger, opt)
+		done := make(chan struct{})
 
-		cms := ConfigMapSaver{
-			klient:    klient,
-			namespace: config.Namespace,
-			name:      config.ConfigMapGeneratedName,
-		}
-
-		gr.Add(func() error {
-			for {
-				select {
-				case <-ctx.Done():
-					return nil
-				case hashring := <-hashringUpdates:
-					cp.UpdateConfig(hashring)
-					if err := cms.SaveHashring(cp.Populate()); err != nil {
-						level.Warn(logger).Log("msg", "failed saving hashring to ConfigMap", "err", err)
-						continue
-					}
-					level.Debug(logger).Log("msg", "Saved a newly generated ConfigMap")
-				case sts := <-StatefulSetUpdates:
-					cp.UpdateStatefulSet(sts)
-					if err := cms.SaveHashring(cp.Populate()); err != nil {
-						level.Warn(logger).Log("msg", "failed saving hashring to ConfigMap", "err", err)
-						continue
-					}
-					level.Debug(logger).Log("msg", "Saved a newly generated ConfigMap")
-				}
-			}
+		g.Add(func() error {
+			return c.run(done)
 		}, func(err error) {
-			cancel()
-			close(hashringUpdates)
-			close(StatefulSetUpdates)
+			level.Info(logger).Log("msg", "shutting down controller")
+			close(done)
 		})
 	}
-
 	level.Info(logger).Log("msg", "starting the controller")
 
-	if err := gr.Run(); err != nil {
+	if err := g.Run(); err != nil {
 		stdlog.Fatal(err)
 	}
 }
 
-type ConfigPopulator struct {
-	namespace    string
-	hashrings    []receive.HashringConfig
-	statefulsets map[string]StatefulSetUpdate
-
-	path   string
-	port   int
-	scheme string
+type options struct {
+	configMapName          string
+	configMapGeneratedName string
+	fileName               string
+	namespace              string
+	path                   string
+	port                   int
+	scheme                 string
+	labelKey               string
+	labelValue             string
 }
 
-func (cp *ConfigPopulator) UpdateConfig(hashrings []receive.HashringConfig) {
-	cp.hashrings = hashrings
+type controller struct {
+	options *options
+	queue   chan struct{}
+	logger  log.Logger
+
+	klient  kubernetes.Interface
+	cmapInf cache.SharedIndexInformer
+	ssetInf cache.SharedIndexInformer
 }
 
-func (cp *ConfigPopulator) UpdateStatefulSet(sts StatefulSetUpdate) {
-	if cp.statefulsets == nil {
-		cp.statefulsets = make(map[string]StatefulSetUpdate, 1)
+func newController(klient kubernetes.Interface, logger log.Logger, o *options) *controller {
+	if logger == nil {
+		logger = log.NewNopLogger()
+	}
+	return &controller{
+		options: o,
+		queue:   make(chan struct{}),
+		logger:  logger,
+
+		klient:  klient,
+		cmapInf: coreinformers.NewConfigMapInformer(klient, o.namespace, resyncPeriod, nil),
+		ssetInf: appsinformers.NewFilteredStatefulSetInformer(klient, o.namespace, resyncPeriod, nil, func(lo *v1.ListOptions) {
+			lo.LabelSelector = labels.Set{o.labelKey: o.labelValue}.String()
+		}),
+	}
+}
+
+func (c *controller) run(stopc <-chan struct{}) error {
+	defer close(c.queue)
+
+	go c.worker()
+
+	go c.cmapInf.Run(stopc)
+	go c.ssetInf.Run(stopc)
+	if err := c.waitForCacheSync(stopc); err != nil {
+		return err
+	}
+	c.cmapInf.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    func(_ interface{}) { c.addWorkItem() },
+		DeleteFunc: func(_ interface{}) { c.addWorkItem() },
+		UpdateFunc: func(_, _ interface{}) { c.addWorkItem() },
+	})
+	c.ssetInf.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    func(_ interface{}) { c.addWorkItem() },
+		DeleteFunc: func(_ interface{}) { c.addWorkItem() },
+		UpdateFunc: func(_, _ interface{}) { c.addWorkItem() },
+	})
+
+	<-stopc
+	return nil
+}
+
+// addWorkItem is non-blocking. If the chan is blocked,
+// then an update will already be processed and we don't
+// need to notify again.
+func (c *controller) addWorkItem() {
+	select {
+	case c.queue <- struct{}{}:
+		return
+	default:
+		return
+	}
+}
+
+// waitForCacheSync waits for the informers' caches to be synced.
+func (c *controller) waitForCacheSync(stopc <-chan struct{}) error {
+	ok := true
+	informers := []struct {
+		name     string
+		informer cache.SharedIndexInformer
+	}{
+		{"ConfigMap", c.cmapInf},
+		{"StatefulSet", c.ssetInf},
+	}
+	for _, inf := range informers {
+		if !cache.WaitForCacheSync(stopc, inf.informer.HasSynced) {
+			level.Error(c.logger).Log("msg", fmt.Sprintf("failed to sync %s cache", inf.name))
+			ok = false
+		} else {
+			level.Debug(c.logger).Log("msg", fmt.Sprintf("successfully synced %s cache", inf.name))
+		}
+	}
+	if !ok {
+		return errors.New("failed to sync caches")
+	}
+	level.Info(c.logger).Log("msg", "successfully synced all caches")
+	return nil
+}
+
+// worker runs a worker thread that just dequeues items, processes them, and
+// marks them done.
+func (c *controller) worker() {
+	for range c.queue {
+		c.sync()
+	}
+}
+
+func (c *controller) sync() {
+	configMap, ok, err := c.cmapInf.GetStore().GetByKey(fmt.Sprintf("%s/%s", c.options.namespace, c.options.configMapName))
+	if !ok || err != nil {
+		level.Warn(c.logger).Log("msg", "could not fetch ConfigMap", "err", err, "name", c.options.configMapName)
+		return
+	}
+	cm := configMap.(*corev1.ConfigMap)
+
+	var hashrings []receive.HashringConfig
+	if err := json.Unmarshal([]byte(cm.Data[c.options.fileName]), &hashrings); err != nil {
+		level.Warn(c.logger).Log("msg", "failed to decode configuration", "err", err)
+		return
 	}
 
-	cp.statefulsets[sts.Name] = sts
+	statefulsets := make(map[string]*appsv1.StatefulSet)
+	for _, obj := range c.ssetInf.GetStore().List() {
+		sts := obj.(*appsv1.StatefulSet)
+		statefulsets[sts.Name] = sts
+	}
+
+	c.populate(hashrings, statefulsets)
+	c.saveHashring(hashrings)
 }
 
-func (cp *ConfigPopulator) Populate() []receive.HashringConfig {
-	hashrings := cp.hashrings
-
-	for i, hashring := range hashrings {
-		if sts, exists := cp.statefulsets[hashring.Hashring]; exists {
+func (c *controller) populate(hashrings []receive.HashringConfig, statefulsets map[string]*appsv1.StatefulSet) {
+	for i, h := range hashrings {
+		if sts, exists := statefulsets[h.Hashring]; exists {
 			var endpoints []string
-			for i := 0; i < sts.Replicas; i++ {
+			for i := 0; i < int(*sts.Spec.Replicas); i++ {
 				endpoints = append(endpoints,
 					// TODO: Make sure this is actually correct
 					fmt.Sprintf("%s://%s-%d.%s.%s:%d/%s",
-						cp.scheme,
+						c.options.scheme,
 						sts.Name,
 						i,
 						sts.Name,
-						cp.namespace,
-						cp.port,
-						strings.TrimPrefix(cp.path, "/")),
+						c.options.namespace,
+						c.options.port,
+						strings.TrimPrefix(c.options.path, "/")),
 				)
 			}
 			hashrings[i].Endpoints = endpoints
 		}
 	}
+}
 
-	return hashrings
+func (c *controller) saveHashring(hashring []receive.HashringConfig) error {
+	buf, err := json.Marshal(hashring)
+	if err != nil {
+		return err
+	}
+
+	cm := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      c.options.configMapGeneratedName,
+			Namespace: c.options.namespace,
+		},
+		Data: map[string]string{
+			c.options.fileName: string(buf),
+		},
+		BinaryData: nil,
+	}
+
+	_, err = c.klient.CoreV1().ConfigMaps(c.options.namespace).Get(c.options.configMapGeneratedName, metav1.GetOptions{})
+	if kerrors.IsNotFound(err) {
+		_, err = c.klient.CoreV1().ConfigMaps(c.options.namespace).Create(cm)
+		return err
+	}
+	if err != nil {
+		return err
+	}
+
+	_, err = c.klient.CoreV1().ConfigMaps(c.options.namespace).Update(cm)
+	return err
 }
--- main_test.go
+++ main_test.go
@@ -1,43 +1,52 @@
 package main
 
 import (
-	"reflect"
+	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/improbable-eng/thanos/pkg/receive"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
-func TestConfigPopulator(t *testing.T) {
-	tests := []struct {
-		name     string
-		updates  func(*ConfigPopulator)
-		expected []receive.HashringConfig
+func intPointer(i int32) *int32 {
+	return &i
+}
+
+func TestController(t *testing.T) {
+	for _, tt := range []struct {
+		name         string
+		hashrings    []receive.HashringConfig
+		statefulsets []*appsv1.StatefulSet
+		expected     []receive.HashringConfig
 	}{
 		{
 			name:     "Empty",
-			updates:  func(cp *ConfigPopulator) {},
 			expected: nil,
-		}, {
+		},
+		{
 			name: "OneHashringNoStatefulSet",
-			updates: func(cp *ConfigPopulator) {
-				cp.UpdateConfig([]receive.HashringConfig{{
-					Hashring: "hashring0",
-				}})
-			},
+			hashrings: []receive.HashringConfig{{
+				Hashring: "hashring0",
+			}},
 			expected: []receive.HashringConfig{{Hashring: "hashring0"}},
 		},
 		{
 			name: "OneHashringOneStatefulSet",
-			updates: func(cp *ConfigPopulator) {
-				cp.UpdateConfig([]receive.HashringConfig{{
-					Hashring:  "hashring0",
-					Tenants:   []string{"foo", "bar"},
-					Endpoints: nil,
-				}})
-				cp.UpdateStatefulSet(StatefulSetUpdate{
-					Name:     "hashring0",
-					Replicas: 3,
-				})
+			hashrings: []receive.HashringConfig{{
+				Hashring: "hashring0",
+				Tenants:  []string{"foo", "bar"},
+			}},
+			statefulsets: []*appsv1.StatefulSet{
+				&appsv1.StatefulSet{
+					ObjectMeta: metav1.ObjectMeta{Name: "hashring0", Labels: map[string]string{"a": "b"}},
+					Spec: appsv1.StatefulSetSpec{
+						Replicas: intPointer(3),
+					},
+				},
 			},
 			expected: []receive.HashringConfig{{
 				Hashring: "hashring0",
@@ -50,78 +59,24 @@ func TestConfigPopulator(t *testing.T) {
 			}},
 		},
 		{
-			name: "OneHashringOneStatefulSetUpdated",
-			updates: func(cp *ConfigPopulator) {
-				cp.UpdateConfig([]receive.HashringConfig{{
-					Hashring:  "hashring0",
-					Tenants:   []string{"foo", "bar"},
-					Endpoints: nil,
-				}})
-				cp.UpdateStatefulSet(StatefulSetUpdate{
-					Name:     "hashring0",
-					Replicas: 3,
-				})
-				cp.UpdateStatefulSet(StatefulSetUpdate{
-					Name:     "hashring0",
-					Replicas: 5,
-				})
-			},
-			expected: []receive.HashringConfig{{
+			name: "OneHashringOneStatefulSetOneBadStatefulSet",
+			hashrings: []receive.HashringConfig{{
 				Hashring: "hashring0",
 				Tenants:  []string{"foo", "bar"},
-				Endpoints: []string{
-					"http://hashring0-0.hashring0.namespace:19291/api/v1/receive",
-					"http://hashring0-1.hashring0.namespace:19291/api/v1/receive",
-					"http://hashring0-2.hashring0.namespace:19291/api/v1/receive",
-					"http://hashring0-3.hashring0.namespace:19291/api/v1/receive",
-					"http://hashring0-4.hashring0.namespace:19291/api/v1/receive",
-				},
 			}},
-		},
-		{
-			name: "OneHashringUpdatedOneStatefulSet",
-			updates: func(cp *ConfigPopulator) {
-				cp.UpdateConfig([]receive.HashringConfig{{
-					Hashring:  "hashring0",
-					Tenants:   []string{"foo", "bar"},
-					Endpoints: nil,
-				}})
-				cp.UpdateStatefulSet(StatefulSetUpdate{
-					Name:     "hashring0",
-					Replicas: 3,
-				})
-				cp.UpdateConfig([]receive.HashringConfig{{
-					Hashring:  "hashring0",
-					Tenants:   []string{"foo", "bar", "baz"},
-					Endpoints: nil,
-				}})
-			},
-			expected: []receive.HashringConfig{{
-				Hashring: "hashring0",
-				Tenants:  []string{"foo", "bar", "baz"},
-				Endpoints: []string{
-					"http://hashring0-0.hashring0.namespace:19291/api/v1/receive",
-					"http://hashring0-1.hashring0.namespace:19291/api/v1/receive",
-					"http://hashring0-2.hashring0.namespace:19291/api/v1/receive",
+			statefulsets: []*appsv1.StatefulSet{
+				&appsv1.StatefulSet{
+					ObjectMeta: metav1.ObjectMeta{Name: "hashring0", Labels: map[string]string{"a": "b"}},
+					Spec: appsv1.StatefulSetSpec{
+						Replicas: intPointer(3),
+					},
 				},
-			}},
-		},
-		{
-			name: "OneHashringOneStatefulSetOneUnusedStatefulSet",
-			updates: func(cp *ConfigPopulator) {
-				cp.UpdateConfig([]receive.HashringConfig{{
-					Hashring:  "hashring0",
-					Tenants:   []string{"foo", "bar"},
-					Endpoints: nil,
-				}})
-				cp.UpdateStatefulSet(StatefulSetUpdate{
-					Name:     "hashring0",
-					Replicas: 3,
-				})
-				cp.UpdateStatefulSet(StatefulSetUpdate{
-					Name:     "hashring123",
-					Replicas: 123,
-				})
+				&appsv1.StatefulSet{
+					ObjectMeta: metav1.ObjectMeta{Name: "hashring123"},
+					Spec: appsv1.StatefulSetSpec{
+						Replicas: intPointer(123),
+					},
+				},
 			},
 			expected: []receive.HashringConfig{{
 				Hashring: "hashring0",
@@ -133,22 +88,106 @@ func TestConfigPopulator(t *testing.T) {
 				},
 			}},
 		},
-	}
-
-	for _, tt := range tests {
+		{
+			name: "OneHashringManyStatefulSets",
+			hashrings: []receive.HashringConfig{{
+				Hashring: "hashring0",
+				Tenants:  []string{"foo", "bar"},
+			}, {
+				Hashring: "hashring1",
+			}},
+			statefulsets: []*appsv1.StatefulSet{
+				&appsv1.StatefulSet{
+					ObjectMeta: metav1.ObjectMeta{Name: "hashring0", Labels: map[string]string{"a": "b"}},
+					Spec: appsv1.StatefulSetSpec{
+						Replicas: intPointer(3),
+					},
+				},
+				&appsv1.StatefulSet{
+					ObjectMeta: metav1.ObjectMeta{Name: "hashring1", Labels: map[string]string{"a": "b"}},
+					Spec: appsv1.StatefulSetSpec{
+						Replicas: intPointer(2),
+					},
+				},
+			},
+			expected: []receive.HashringConfig{{
+				Hashring: "hashring0",
+				Tenants:  []string{"foo", "bar"},
+				Endpoints: []string{
+					"http://hashring0-0.hashring0.namespace:19291/api/v1/receive",
+					"http://hashring0-1.hashring0.namespace:19291/api/v1/receive",
+					"http://hashring0-2.hashring0.namespace:19291/api/v1/receive",
+				},
+			}, {
+				Hashring: "hashring1",
+				Endpoints: []string{
+					"http://hashring1-0.hashring1.namespace:19291/api/v1/receive",
+					"http://hashring1-1.hashring1.namespace:19291/api/v1/receive",
+				},
+			}},
+		},
+	} {
 		t.Run(tt.name, func(t *testing.T) {
-			cp := &ConfigPopulator{
-				namespace: "namespace",
-				path:      "/api/v1/receive",
-				port:      19291,
-				scheme:    "http",
+			klient := fake.NewSimpleClientset()
+			opts := &options{
+				labelKey:               "a",
+				labelValue:             "b",
+				fileName:               "hashrings.json",
+				configMapName:          "original",
+				configMapGeneratedName: "generated",
+				namespace:              "namespace",
+				path:                   "/api/v1/receive",
+				port:                   19291,
+				scheme:                 "http",
+			}
+			c := newController(klient, nil, opts)
+			stop := make(chan struct{})
+			defer close(stop)
+
+			go func() {
+				if err := c.run(stop); err != nil {
+					t.Fatalf("got unexpected error: %v", err)
+				}
+			}()
+
+			buf, err := json.Marshal(tt.hashrings)
+			if err != nil {
+				t.Fatalf("got unexpected error marshaling initial hashrings: %v", err)
+			}
+			cm := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      opts.configMapName,
+					Namespace: opts.namespace,
+				},
+				Data: map[string]string{
+					opts.fileName: string(buf),
+				},
+				BinaryData: nil,
+			}
+			if _, err := klient.CoreV1().ConfigMaps(opts.namespace).Create(cm); err != nil {
+				t.Fatalf("got unexpected error creating ConfigMap: %v", err)
 			}
 
-			tt.updates(cp)
+			for _, sts := range tt.statefulsets {
+				if _, err := klient.AppsV1().StatefulSets(opts.namespace).Create(sts); err != nil {
+					t.Fatalf("got unexpected error creating StatefulSet: %v", err)
+				}
+			}
 
-			config := cp.Populate()
-			if !reflect.DeepEqual(config, tt.expected) {
-				t.Errorf("the expected config does not match the actual config\ncase:\t%q\ngiven:\t%+v\nexpected:\t%+v\n", tt.name, config, tt.expected)
+			// Reconciliation is async, so we need to wait a bit.
+			<-time.After(250 * time.Millisecond)
+			cm, err = klient.CoreV1().ConfigMaps(opts.namespace).Get(opts.configMapGeneratedName, metav1.GetOptions{})
+			if err != nil {
+				t.Fatalf("got unexpected error getting ConfigMap: %v", err)
+			}
+
+			buf, err = json.Marshal(tt.expected)
+			if err != nil {
+				t.Fatalf("got unexpected error marshaling expected hashrings: %v", err)
+			}
+
+			if cm.Data[opts.fileName] != string(buf) {
+				t.Errorf("the expected config does not match the actual config\ncase:\t%q\ngiven:\t%+v\nexpected:\t%+v\n", tt.name, cm.Data[opts.fileName], string(buf))
 			}
 		})
 	}
